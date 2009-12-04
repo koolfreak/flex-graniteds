ff0e091cae27cf852c96a86ab4db4f1049a2bd4d
eman.nollase <eman.nollase@c1f8c7e8-e074-11de-a668-6fd11605f5ce>
2009-12-04 06:18:18 +0000

    added spring-security with dao authentication provider

--- gds-tide-server/src/main/java/org/graniteds/tide/model/User.java
+++ gds-tide-server/src/main/java/org/graniteds/tide/model/User.java
@@ -1,58 +1,146 @@
 package org.graniteds.tide.model;
 
-public class User {	
+import java.io.Serializable;
 
-	private String name;
-	private String login;
-	
-	public User() {
-		super();
-	}
-	
-	public User(String login, String name) {
-		this.login = login;
-		this.name = name;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.hibernate.annotations.Proxy;
+
+@Entity
+@Proxy(lazy = false)
+@Table(name = "tbl_users")
+public class User implements Serializable
+{
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Basic
+	@Column
+	private String username;
+
+	@Basic
+	@Column
+	private String password;
+
+	@Basic
+	@Column
+	private String firstName;
+
+	@Basic
+	@Column
+	private String lastName;
+
+	@Basic
+	@Column
+	private String email;
+
+	@Basic
+	@Column
+	private String contactNumber;
+
+	@Basic
+	@Column(columnDefinition = "tinyint(1)")
+	private boolean enabled;
+
+	public Long getId()
+	{
+		return id;
 	}
 
-	public String getName() {
-		return name;
+	public void setId(Long id)
+	{
+		this.id = id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getUsername()
+	{
+		return username;
 	}
 
-	public String getLogin() {
-		return login;
+	public void setUsername(String username)
+	{
+		this.username = username;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getContactNumber()
+	{
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber)
+	{
+		this.contactNumber = contactNumber;
+	}
+
+	public boolean isEnabled()
+	{
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled)
+	{
+		this.enabled = enabled;
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((login == null) ? 0 : login.hashCode());
-		return result;
+	public boolean equals(Object obj)
+	{
+		return EqualsBuilder.reflectionEquals(this, obj);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final User other = (User) obj;
-		if (login == null) {
-			if (other.login != null)
-				return false;
-		} else if (!login.equals(other.login))
-			return false;
-		return true;
+	public int hashCode()
+	{
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
-	
+
 }
--- gds-tide-server/src/main/java/org/graniteds/tide/service/album/AlbumManager.java
+++ gds-tide-server/src/main/java/org/graniteds/tide/service/album/AlbumManager.java
@@ -3,10 +3,12 @@ package org.graniteds.tide.service.album;
 import java.util.List;
 
 import org.graniteds.tide.model.Album;
+import org.springframework.security.annotation.Secured;
 
 public interface AlbumManager
 {
-
+	
+	@Secured({"ROLE_EDIT"})
 	void save(Album album);
 	
 	void update(Album album);
